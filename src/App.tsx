78856aa014bbffb3a1e3caee483c6057fef22007
owengiri20 <owengiri20@gmail.com>
2023-06-20 23:55:55 +0800

    planet graphic

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import React from "react"
 import { Route, Switch } from "react-router-dom"
 import "./App.css"
 import Logo from "./assets/logo.png"
+import Moon from "./assets/moon.png"
+
 import { AppWrapper } from "./common/appWrapper"
 import { GameScreen } from "./screens/GameScreen"
 import { COLOURS } from "./game/CommonStyles"
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
 
 function App() {
     const under1100Height = useMediaQuery("(max-height:1100px)")
@@ -23,21 +26,52 @@ function App() {
                 <Container maxWidth={"xl"} style={{ maxWidth: "1200px", transform: under1100Height ? "scale(.7)" : "scale(.9)" }}>
                     <Box
                         style={{
-                            marginTop: under1100Height ? "0" : "2rem",
                             display: "flex",
-                            padding: "1rem",
-                            alignItems: "center",
-                            borderRadius: "30px",
-                            background: COLOURS.darkBrown,
-                            color: COLOURS.lightBrown,
-                            width: "fit-content",
+                            justifyContent: "space-between",
+                            alignContent: "center",
                         }}
                     >
-                        <img src={Logo} alt="TrekTyper Logo" height={"80px"} />
-                        <Box sx={{ fontSize: "30px" }}>TrekTyper</Box>
+                        <Box
+                            style={{
+                                marginTop: under1100Height ? "0" : "2rem",
+                                display: "flex",
+                                padding: "1rem",
+                                alignItems: "center",
+                                borderRadius: "30px",
+                                background: COLOURS.darkBrown,
+                                color: COLOURS.lightBrown,
+                                width: "fit-content",
+                            }}
+                        >
+                            <img src={Logo} alt="TrekTyper Logo" height={"80px"} />
+                            <Box sx={{ fontSize: "30px" }}>TrekTyper</Box>
+                        </Box>
+
+                        <Box
+                            style={{
+                                marginTop: under1100Height ? "0" : "2rem",
+                                display: "flex",
+                                padding: "1rem",
+                                alignItems: "center",
+                                borderRadius: "30px",
+                                background: COLOURS.darkBrown,
+                                color: COLOURS.lightBrown,
+                                width: "fit-content",
+                                cursor: "pointer",
+                            }}
+                        >
+                            <AccountCircleIcon
+                                // onClick={() => setOptionsMenuOpen(true)}
+                                style={{ marginRight: "1rem", color: COLOURS.lightBrown, fontSize: "35px", cursor: "pointer" }}
+                            />
+                            <Box sx={{ fontSize: "20px" }}>Sign in</Box>
+                        </Box>
                     </Box>
+
                     <Switch>
                         <Route path="/" component={GameScreen} exact />
+                        <Route path="/signin" component={GameScreen} exact />
+                        <Route path="/signup" component={GameScreen} exact />
                     </Switch>
                 </Container>
                 <Box
@@ -50,11 +84,13 @@ function App() {
                         paddingBottom: "1rem",
                     }}
                 >
-                    <Box style={{ color: COLOURS.lightBrown, cursor: "pointer" }}>
+                    <Box style={{ color: COLOURS.lightBrown, background: COLOURS.darkBrown, padding: ".5rem", borderRadius: "7px", cursor: "pointer" }}>
                         <a href="https://owengiri.dev/" target="_blank">
                             © 2023 Owen Giri
                         </a>
                     </Box>
+
+                    <img style={{ position: "absolute", zIndex: -99, height: "70vh", bottom: "-40vh" }} src={Moon} alt="TrekTyper Logo" height={"80px"} />
                 </Box>
             </>
         </AppWrapper>
