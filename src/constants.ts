8e8d9e5893cdef7eaafb548a3700b0dd5f70b5c6
owengiri20 <owengiri20@gmail.com>
2023-06-26 23:41:14 +0800

    temp

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 let ENV = "PROD"
 
-// const TEST_URL = "https://api-typinggame.owengiri.dev/api"
-const TEST_URL = "https://urchin-app-8kf9c.ondigitalocean.app/api"
+const TEST_URL = "https://api-typinggame.owengiri.dev/api"
+// const TEST_URL = "https://urchin-app-8kf9c.ondigitalocean.app/api"
 
 export const BASE_API_URL = ENV === "DEV" ? "http://localhost:8080/api" : TEST_URL
 
