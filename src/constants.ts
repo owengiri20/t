697cc0abfbe35fd6ac0e3c4e95649c3e32a6947f
owengiri20 <owengiri20@gmail.com>
2023-06-26 14:14:45 +0800

    env update

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
-export const BASE_API_URL = "http://localhost:8080/api"
+const ENV = process.env.ENV ?? "DEV"
+export const BASE_API_URL = ENV === "DEV" ? "http://localhost:8080/api" : process.env.BASE_API_URL
+
+console.log(process.env.BASE_API_URL)
