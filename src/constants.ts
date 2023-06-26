15a098d2775974c9247b99769ecea7789b86129d
owengiri20 <owengiri20@gmail.com>
2023-06-26 14:21:58 +0800

    workaround env

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
-const ENV = process.env.ENV ?? "DEV"
-export const BASE_API_URL = ENV === "DEV" ? "http://localhost:8080/api" : process.env.BASE_API_URL
+let ENV = "PROD"
+
+const TEST_URL = "https://urchin-app-8kf9c.ondigitalocean.app/api"
+
+export const BASE_API_URL = ENV === "DEV" ? "http://localhost:8080/api" : TEST_URL
 
 console.log(process.env.BASE_API_URL)
