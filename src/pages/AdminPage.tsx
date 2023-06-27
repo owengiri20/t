9752d15567d9bc1017b755d95ec94fead7caa686
owengiri20 <owengiri20@gmail.com>
2023-06-27 16:11:45 +0800

    modal when logging out

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@material-ui/core"
+import { Layout } from "./Layout"
+import React from "react"
+
+export const AdminPage = () => {
+    return (
+        <Layout>
+            <Box>
+                <h1>hello from admin</h1>
+            </Box>
+        </Layout>
+    )
+}
