01110c16a8b230fe6b781570c0a6faf55b61a14a
owengiri20 <owengiri20@gmail.com>
2023-07-07 21:56:39 +0800

    re render issue

--- src/containers/game.ts
+++ src/containers/game.ts
@@ -1,4 +1,6 @@
 import { atom, useAtom } from "jotai"
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
 
 export interface GameState {
     status: "finished" | "idle" | "playing"
@@ -12,6 +14,12 @@ export const useGame = () => {
     const [gameState, setGameState] = useAtom(gameStateAtom)
 
     const hideSettings = gameState.status !== "idle"
+
+    const location = useLocation()
+
+    useEffect(() => {
+        setGameState((prevState) => ({ ...prevState, status: "idle" }))
+    }, [location, setGameState])
 
     return {
         gameState,
